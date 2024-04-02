44c03a961571c5fcdb1cfae413ce3520b0ea840b
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-02 17:54:27 +0200

    feat: snack 9

---
+++ JSnack 9/js/script.js
@@ -0,0 +1,10 @@
+let total = 0
+
+for (i = 1; i < 11; i++) {
+    const number = parseInt(`${i}`)
+    total = total + number;
+    console.log(total);
+    media = total / parseInt(`${i}`)
+    console.log(media);
+}
+
