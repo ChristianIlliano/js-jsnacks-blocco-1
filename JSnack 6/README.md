38f545a25a6986a58f32e745c89742ed37bfbf94
ChristianIlliano <grafica.illiano@gmail.com>
2024-04-02 16:42:27 +0200

    feat: snack 6

---
+++ JSnack 6/README.md
@@ -0,0 +1,3 @@
+JSnack 6
+Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall'utente.
---
+++ JSnack 6/js/script.js
@@ -0,0 +1,4 @@
+const number = prompt(`dimmi un numero`)
+
+let cubicRoot =Math.cbrt(number).toFixed(2)
+console.log(cubicRoot);
